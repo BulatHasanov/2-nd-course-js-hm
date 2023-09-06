722b449e22eb137a1154d1ec3df0b9e143cd8f32
Bulat <hasanovb123@gmail.com>
2023-09-06 14:38:51 +0300

    Пройдено занятие "Функции"

--- index.js
+++ index.js
@@ -135,47 +135,177 @@ switch (monthNumber) {
 // }
 
                              //Занятие 2.4
+// //Задание 1
+// let i = 0;
+
+// while (i<2) {
+//     console.log('Привет');
+//     i++;
+// }
+// // Задание 2
+// let I = 1;
+// while (I<=5) {
+//     console.log(I);
+//     I++;
+// }
+
+// //Задание 3
+// for (let i = 7; i < 23; i++) {
+//     console.log(i);  
+// }
+
+// //Задание 4
+// const obj = {
+//     'Коля':200,
+//     'Вася':300,
+//     'Петя':400
+// }
+// for (let name in obj) {
+//     console.log(`${name} зарабатывает ${obj[name]}`)
+//     }
+
+// //Задание 5
+// for (let n = 1000, num = 0; n > 50; ) {
+//     num++;
+//     n = n/2;
+//     if (n<50) {
+//         console.log(n);
+//         console.log(num); 
+//     }
+// }
+
+// //Задание 6
+// for (let dayOfTheWeek = 4; Number(dayOfTheWeek) < 31;) {
+//     console.log(`Сегодня пятница, ${dayOfTheWeek}-е число. Необходимо подготовить отчет.`);
+//     dayOfTheWeek = dayOfTheWeek + 7;
+// }
+
+                           // Занятие 2.5
 //Задание 1
-let i = 0;
+// Напишите функцию, которая возвращает меньшее из 2 чисел. Например, из двух чисел — 8 и 4 — функция должна вернуть 4. Если передадим 6 и 6, то функция должна вернуть 6.
 
-while (i<2) {
-    console.log('Привет');
-    i++;
-}
+// function minNumb(a , b) {
+//     if (a < b) {
+//         return a
+//     } else { 
+//         return b
+//     }
+// }
+// minNumb(8 , 4);
+// minNumb(6 , 6);
+// console.log(minNumb(8 , 4));
+// console.log(minNumb(6 , 6));
+
 // Задание 2
-let I = 1;
-while (I<=5) {
-    console.log(I);
-    I++;
-}
+// Напишите функцию, которая принимает число, а возвращает строку 'Число четное', если число четное, 'Число нечетное' — если нечетное.
 
-//Задание 3
-for (let i = 7; i < 23; i++) {
-    console.log(i);  
-}
+// function evenNotEven(n) {
+//     if (n % 2 == 0) {
+//         return ('Число четное')
+//     } else { 
+//         return ('Число нечетное')
+//     }
+// }
+// evenNotEven(4);
+// evenNotEven(5);
+// evenNotEven(1);
+// console.log(evenNotEven(4));
+// console.log(evenNotEven(5));
+// console.log(evenNotEven(1));
 
-//Задание 4
-const obj = {
-    'Коля':200,
-    'Вася':300,
-    'Петя':400
-}
-for (let name in obj) {
-    console.log(`${name} зарабатывает ${obj[name]}`)
-    }
+// Задание 3
+// 3.1. Напишите функцию, которая принимает параметром число и выводит в консоль квадрат этого числа.
 
-//Задание 5
-for (let n = 1000, num = 0; n > 50; ) {
-    num++;
-    n = n/2;
-    if (n<50) {
-        console.log(n);
-        console.log(num); 
-    }
-}
+// let squareNumb = (n) => n ** 2;
+// squareNumb(10);
+// console.log(squareNumb(10));
 
-//Задание 6
-for (let dayOfTheWeek = 4; Number(dayOfTheWeek) < 31;) {
-    console.log(`Сегодня пятница, ${dayOfTheWeek}-е число. Необходимо подготовить отчет.`);
-    dayOfTheWeek = dayOfTheWeek + 7;
-}
+// 3.2. Напишите функцию, которая принимает параметром число и возвращает квадрат этого числа.
+
+// let numbSquare = function (n) {
+//     return n ** 2;
+// }
+// numbSquare(4);
+// console.log(numbSquare(4));
+
+// Задание 4
+// Создайте функцию, которая:
+//  Спрашивает у пользователя, сколько ему лет.
+//  Если пользователь вводит отрицательное число, выводит на экран «Вы ввели неправильное значение».
+//  Если пользователь введет число от 0 до 12, выводит на экран «Привет, друг!».
+//  Если пользователь введет число больше или равно 13, выводит на экран «Добро пожаловать!».
+
+// let age = function (n = prompt('Сколько вам лет?') ) {
+//     if ((n > 0) && (n <= 12)) {
+//         alert('Привет, друг!')
+//     } else if(n >= 13){
+//         alert('Добро пожаловать!')
+//     } else {alert('Вы ввели неправильное значение')}
+// }
+// age();
+
+// Задание 5
+// Напишите функцию, которая на вход принимает 2 числа:
+//  Проверяет, являются ли переданные параметры корректными числами. (Преобразование типов и 
+//      isNaN() помогут.)
+//  Если нет, то вернуть строку 'Одно или оба значения не являются числом'.
+//  Если оба параметра — числа, то вернуть произведение данных чисел.
+
+// let num = function (d , c) {
+//     if (typeof d === "string") {
+//         return ('Одно или оба значения не являются числом');
+//     } else if (typeof c === "string") {
+//         return ('Одно или оба значения не являются числом');
+//     } 
+//       else {
+//         return d * c;
+//     } 
+// }
+// num(8 , 32);
+// num(NaN , '3');
+// num(24, 'abc')
+// console.log(num(8 , 32));
+// console.log(num(1028 , 'r'));
+// console.log(num(24, 'abc'));
+
+// Задание 6
+// Напишите функцию, которая спрашивает у пользователя число, проверяет его на корректность, после чего возводит его в куб и возвращает следующую строку: 'n в кубе равняется <получившееся значение>'
+    // Если переданный параметр не число, вернуть строку 'Переданный параметр не является числом'.
+
+    // Проверьте самостоятельно, что данная функция работает корректно и выводит правильный результат с параметрами от 0 до 10 включительно.
+    
+    // let userAnswer = prompt('Введите число от 0 до 10');
+    // let cubedNumb = function (userAnswer) {
+    //     if (isNaN(userAnswer)) {
+    //         return ('Переданный параметр не является числом');
+    //     } else {
+    //         return (`${userAnswer} в кубе равняется ${userAnswer ** 3}`);
+    //     } 
+    // }
+    // cubedNumb();
+    // console.log(cubedNumb(userAnswer));
+
+// Задание 7
+// Создайте объекты circle1 и circle2 со свойством radius. У объектов должен быть методам getArea, которое возвращает площадь круга через радиус, а также getPerimet, который возвращает периметр окружности.
+
+// function circleArea() {
+//     return (Math.PI * (this.radius ** 2))
+// }
+
+// function circlePerimeter() {
+//     return (Math.PI * 2 * this.radius)
+// }
+// const circle1 = {
+//     radius: 9,
+//     getArea: circleArea,
+//     getPerimeter: circlePerimeter,
+// }
+// const circle2 = {
+//     radius: 10,
+//     getArea: circleArea,
+//     getPerimeter: circlePerimeter,
+// }
+// console.log(circle1.getArea());
+// console.log(circle1.getPerimeter());
+// console.log(circle2.getArea());
+// console.log(circle2.getPerimeter());
